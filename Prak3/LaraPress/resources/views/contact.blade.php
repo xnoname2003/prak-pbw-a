8c674547b4c55a32174d4c3615f3e1bcaa6f6bf3
Chaerul Cahyadi <chaeruladi15@gmail.com>
2025-10-03 10:36:26 +0700

    Prak3-LaraPress

---
+++ Prak3/LaraPress/resources/views/contact.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Kontak Kami - LaraPress</title>
+</head>
+
+<body>
+    <h1>Kontak LaraPress</h1>
+    <p>Nama : Chaerul Cahyadi</p>
+    <p>NPM : 4523210120</p>
+    <p>Email : chaerul4523120@univpancasila.ac.id</p>
+    <p>No.Hp : 08976446653</p>
+    <a href="/about">Lihat Halaman Tentang Kami</a>
+    <br>
+    <a href="/">Kembali ke Halaman Utama</a>
+</body>
+
+</html>
---
+++ Prak3/LaraPress/resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Selamat Datang di LaraPress</title>
+</head>
+<body>
+    <h1>Selamat Datang di Blog LaraPress</h1>
+    <p>Ini adalah halaman utama dari aplikasi blog kita.</p>
+    <a href="/about">Lihat Halaman Tentang Kami</a>
+    <br>
+    <a href="/">Kembali ke Halaman Utama</a>
+</body>
+</html>
